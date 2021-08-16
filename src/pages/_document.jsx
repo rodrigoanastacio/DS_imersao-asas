02facc602c27168bd6b9a587f677f112e205769d
Anastacio <rodrigo.anastacio@wundermanthompson.com>
2021-08-16 01:34:15 -0300

    Add script GTM e Pixel Facebook

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -20,6 +20,48 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700;900&family=Roboto:wght@100;400&display=swap"
             rel="stylesheet"
           />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js')
+                fbq('init', '1058401671286484');
+                fbq('track', 'PageView');`,
+            }}
+          />
+
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `(<img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id=1058401671286484&ev=PageView&noscript=1"`,
+            }}
+          />
+
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-KQWKKDL');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
+
+          {/* Google Tag Manager (noscript) */}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQWKKDL" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
         </Head>
         <body>
           <Main />
